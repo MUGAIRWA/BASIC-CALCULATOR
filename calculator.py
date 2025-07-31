49edaf0104e313b48842461c2d9afb73c2cb37a0
MUGAIRWA <agalatevinmugairwa@gmail.com>
2025-07-31 23:22:35 +0300

    create variables and user prompted to enter values and operation

---
+++ calculator.py
@@ -0,0 +1,4 @@
+# Ask the user to input two numbers and an operation
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
